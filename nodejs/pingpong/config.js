b815becdf9b243d0b5c3834f79b6de1f87dff615
tdpnet <cto@4da.se>
2026-09-17 19:23:17 +0200

    pingpong

--- nodejs/pingpong/config.js
+++ nodejs/pingpong/config.js
@@ -1,37 +1,15 @@
 module.exports = {
-    pinpon: {
-        host: 'ws://213.199.34.255:5000', 
-        credential: {
-                accesskey: 'PINPON',
-                password: '000000',
-                address: 'pinpon.aladino.maya.4da',
-                context: {}
-        }
-    },
     ponpin: {
-        host: 'ws://213.199.34.255:5000', 
+        host: 'ws://213.199.58.37:5000', 
         credential: {
                 accesskey: 'PONPIN',
                 password: '000000',
                 address: 'ponpin.aladino.maya.4da',
                 context: {}
         },
-        peers: {
-            pinpon: 'pinpon.aladino.maya.4da'
-        }
-    },
-    punpin: {
-        host: 'ws://213.199.34.255:5001', 
-        credential: {
-                accesskey: 'PUNPIN',
-                password: '000000',
-                address: 'punpin.aladino.maya.4da',
-                context: {}
-        },
+
         peers: {
             pinpon: 'pinpon.aladino.maya.4da'
         }
     }
-
 }
-
--- nodejs/pingpong/index.js
+++ nodejs/pingpong/index.js
@@ -2,43 +2,6 @@ const config = require('./config')
 
 new class extends require('./system'){
     constructor(){
-        super(config.pinpon)
-    }
-
-    onDenied(data){
-        console.log('denied', data)
-    }
-
-    onGranted(data){
-        console.log('granted', data)
-    }
-
-    onEvent(id, packet){
-        const to = packet.pearing.from;
-        console.log('event', id, to, Date.now())
-        switch(id){
-            case 'ping': setTimeout(() => this.notify(this.address,to, 'pong', 0), 2000); break;
-        }
-    }
-
-    onResponse(id, packet){
-        console.log('response', id, packet.peering?.from,  Date.now())
-        switch(id){
-            case 'ping': setTimeout(() => this.response(packet, 'pong', 0), 2000); break;
-        }
-    }
-
-    onRequest(id, packet){
-        console.log('request', id, packet.peering?.from,  Date.now())
-        switch(id){
-            case 'ping': setTimeout(() => this.response(packet, 'pong',0), 1000); break;
-        }
-
-    }
-}
-
-new class extends require('./system'){
-    constructor(){
         super(config.ponpin)
     }
 
@@ -48,7 +11,8 @@ new class extends require('./system'){
 
     onGranted(data){
         console.log('granted', data)
-        this.request(this.address, '', 'ping', 0)
+        this.request(this.address, this.peers.pinpon, 'ping', 0)
+        //this.notify(this.address, 'pinpon.aladino.maya.4da', 'pong', 0)
     }
 
     onEvent(id, packet){
@@ -60,54 +24,14 @@ new class extends require('./system'){
     }
 
     onResponse(id, packet){
-        console.log('response',this.address,  id, packet.peering?.from,  Date.now())
+        console.log('response', id, Date.now())
         switch(id){
-            case 'pong': setTimeout(() => this.response(packet, 'ping', 1), 1000); break;
+            case 'pong': setTimeout(() => this.response(packet, 'ping', 1), 2000); break;
         }
     }
 
     onRequest(id, packet){
-        console.log('request', this.address,  id, packet.peering?.from,  Date.now())
-        switch(id){
-            case 'pong': setTimeout(() => this.response(packet, 'ping', 1), 1000); break;
-        }
-
-    }
-}
-
-
-
-new class extends require('./system'){
-    constructor(){
-        super(config.punpin)
-    }
-
-    onDenied(data){
-        console.log('denied', data)
-    }
-
-    onGranted(data){
-        console.log('granted', data)
-        this.request(this.address, '', 'ping', 0)
-    }
-
-    onEvent(id, packet){
-        const to = packet.pearing.from;
-        console.log('event', id, to, Date.now())
-        switch(id){
-            case 'ping': setTimeout(() => this.notify(this.address,to, 'pong', 0), 2000); break;
-        }
-    }
-
-    onResponse(id, packet){
-        console.log('response', this.address, id, packet.peering?.from,  Date.now())
-        switch(id){
-            case 'pong': setTimeout(() => this.response(packet, 'ping', 1), 1000); break;
-        }
-    }
-
-    onRequest(id, packet){
-        console.log('request', id, packet.peering?.from,  Date.now())
+        console.log('request', id, Date.now())
         switch(id){
             case 'pong': setTimeout(() => this.response(packet, 'ping', 1), 1000); break;
         }
