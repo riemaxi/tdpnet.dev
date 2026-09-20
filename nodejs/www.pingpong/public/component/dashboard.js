3b7b57dd495cc7df665e72e428e8063f99b90a5a
tdpnet <cto@4da.se>
2026-09-20 09:26:46 +0200

    pingpong

--- nodejs/www.pingpong/public/component/dashboard.js
+++ nodejs/www.pingpong/public/component/dashboard.js
@@ -9,10 +9,52 @@ export class Dashboard extends Component {
   render() {
     this.shadowRoot.innerHTML = `
       <div part="dashboard-wrapper">
-        <chat-header id="header" part="header"></chat-header>
-        <peer-list id="peer-list" part="peer-list"></peer-list>
-        <message-list id="message-list" part="message-list"></message-list>
-        <message-editor id="message-editor" part="message-editor"></message-editor>
+        <chat-header 
+          id="header" 
+          part="header"
+          exportparts="
+            container: header-container,
+            branding: header-branding,
+            logo: header-logo,
+            username: header-username,
+            signoff-btn: header-signoff-btn
+          "
+        ></chat-header>
+        
+        <peer-list 
+          id="peer-list" 
+          part="peer-list"
+          exportparts="
+            container: peers-container,
+            title: peers-title,
+            select-all-item: peers-select-all-item,
+            peer-item: peers-peer-item,
+            checkbox: peers-checkbox,
+            list: peers-list
+          "
+        ></peer-list>
+        
+        <message-list 
+          id="message-list" 
+          part="message-list"
+          exportparts="
+            container: messages-container,
+            empty-state: messages-empty-state,
+            msg: messages-msg,
+            msg-self: messages-msg-self,
+            msg-author: messages-msg-author
+          "
+        ></message-list>
+        
+        <message-editor 
+          id="message-editor" 
+          part="message-editor"
+          exportparts="
+            container: editor-container,
+            input: editor-input,
+            send-btn: editor-send-btn
+          "
+        ></message-editor>
       </div>
     `;
     this.listen();
